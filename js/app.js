3ddcf1a78638a30f168708b021ffc19a2bb77b31
McHenry, Zachary <Zachary_McHenry@intuit.com>
2014-07-31 15:56:17 -0700

    Added image tiles and top nav bar with logo

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var app = angular.module('imgur', []);
 app.service("imageService", function($http, $q) {
+    
     return ({
         getGallery: getGallery
     });
@@ -8,6 +9,14 @@ app.service("imageService", function($http, $q) {
         var request = $http({
             method:"get",
             url: "https://api.imgur.com/3/gallery/hot/viral/0"
+        });
+        return (request.then(handleSuccess, handleError));
+    }
+
+    function getAlbum(id) {
+        var request = $http({
+            method: "get",
+            url: ("https://api.imgur.com/3/gallery/album/" + id)
         });
         return (request.then(handleSuccess, handleError));
     }
@@ -27,29 +36,58 @@ app.service("imageService", function($http, $q) {
 
 
 app.controller('GalleryCtrl', function($scope, imageService) {
-    
-
+    $scope.images = [];
+    var numberOfRows = 10;
+    var imgsPerRow = 5;
     $scope.getGallery = function() {
-        imageService.getGallery().then(loadRemoteData,
+        imageService.getGallery().then(function(images){
+                                            applyRemoteData(images);
+                                        },
                                         function() {
-                                            console.log("failure");}
+                                            console.log("Failure getting gallery");}
                                         );
     };
 
+    function checkForAlbums() {
+        angular.forEach($scope.images.data, function(image) {
+            if(image.is_album) {
+                image.link = "http://i.imgur.com/" + image.cover + "m.jpg";
+            } else {
+                image.link = image.link.replace(image.id, image.id + "m");
+            }
+        });
+    }
+
     function applyRemoteData(images) {
         $scope.images = images;
+        checkForAlbums();
+        rowify(numberOfRows, imgsPerRow);
         console.log($scope.images);
         console.log("Success");
     }
 
-    function loadRemoteData() {
-        imageService.getGallery().then(function(images) {
-            applyRemoteData(images);
-        });
+    function rowify(rows, iPerRow) {
+        var images = [];
+        var cnt = 0;
+        for(var i = 0; i < rows; i++) {
+            var image_row = [];
+            for(var j = 0; j < iPerRow; j++) {
+                image_row.push($scope.images.data[cnt++]);
+            }
+            images.push(image_row);
+        }
+        $scope.images = images;
     }
+
     $scope.images = $scope.getGallery();
     console.log($scope.images);
 });
 
+app.directive("imageGrid", function() {
+    return {
+        restrict: "E",
+        templateUrl: "partials/image-grid.html"
+    };
+});
 
 
